6302bf5b84034ff85ed296d86efeaf944e568728
stwarkentin <stwarkentin@uos.de>
2020-07-24 17:54:32 +0200

    Add trial_number column to clean data

--- analyses/01_pilot/01_data_preprocessing.R
+++ analyses/01_pilot/01_data_preprocessing.R
@@ -8,7 +8,7 @@ raw_data %>%
   # exclude answers that were given in less than 300ms
   filter(RT >= 300) %>%
   # select the relevant columns, dropping the rest
-  select(submission_id,scenario,predicate,sentence_frame,RT,response) %>%
+  select(submission_id,trial_number,scenario,predicate,sentence_frame,RT,response) %>%
   # write to csv file
   write_csv("01_pilot/data/02_clean_data.csv")
 
